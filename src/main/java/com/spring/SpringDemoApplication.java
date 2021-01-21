2b79194ecbdad4282a958694a089172daacdeeac
jonssonyan <yz808@outlook.com>
2021-01-21 12:34:45 +0800

    first commit

---
+++ src/main/java/com/spring/SpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoApplication.class, args);
+    }
+
+}
