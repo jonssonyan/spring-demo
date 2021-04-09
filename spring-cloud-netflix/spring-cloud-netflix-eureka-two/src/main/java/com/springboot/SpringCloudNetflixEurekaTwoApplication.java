fec2420d9882e647b198caaad42af428d9a598ce
YanZhuangZhuang <yz808@outlook.com>
2021-04-09 16:52:31 +0800

    fix: spring cloud netflix

---
+++ spring-cloud-netflix/spring-cloud-netflix-eureka-two/src/main/java/com/springboot/SpringCloudNetflixEurekaTwoApplication.java
@@ -0,0 +1,15 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudNetflixEurekaTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudNetflixEurekaTwoApplication.class, args);
+    }
+
+}
