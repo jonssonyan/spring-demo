e8294d22b576cea25d232e4a4b353528a96a82c0
jonssonyan <yz808@outlook.com>
2021-04-11 18:03:14 +0800

    spring cloud alibaba nacos config

---
+++ spring-cloud-alibaba/spring-cloud-alibaba-nacos-config/src/test/java/com/springcloud/SpringCloudAlibabaNacosConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAlibabaNacosConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
