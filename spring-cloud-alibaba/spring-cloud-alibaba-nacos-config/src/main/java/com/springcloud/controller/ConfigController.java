b284a8d7e811485c5b01a0b146a8dd9e533df0b9
jonssonyan <yz808@outlook.com>
2021-04-11 23:36:30 +0800

    spring cloud alibaba nacos config

--- spring-cloud-alibaba/spring-cloud-alibaba-nacos-config/src/main/java/com/springcloud/controller/ConfigController.java
+++ spring-cloud-alibaba/spring-cloud-alibaba-nacos-config/src/main/java/com/springcloud/controller/ConfigController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/config")
+// 实现配置自动更新
 @RefreshScope
 public class ConfigController {
     @Value("${useLocalCache:false}")
