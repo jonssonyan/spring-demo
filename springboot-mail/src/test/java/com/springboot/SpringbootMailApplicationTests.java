20062777be400bc77cb54d4df4d1eccc66d3a6e0
Jonsson <yz808@outlook.com>
2021-06-01 01:26:37 +0800

    Spring Boot Mail

--- springboot-mail/src/test/java/com/springboot/SpringbootMailApplicationTests.java
+++ springboot-mail/src/test/java/com/springboot/SpringbootMailApplicationTests.java
@@ -2,13 +2,10 @@ package com.springboot;
 
 import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 class SpringbootMailApplicationTests {
     @Autowired
     private StringEncryptor encryptor;
