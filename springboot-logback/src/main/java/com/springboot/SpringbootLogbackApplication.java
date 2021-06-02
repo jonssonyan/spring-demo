1ef60a55f6c7fdfb4e9c93be21a164b4e7a80816
Jonsson <yz808@outlook.com>
2021-06-02 23:29:20 +0800

    Spring Boot Logback

--- springboot-logback/src/main/java/com/springboot/SpringbootLogbackApplication.java
+++ springboot-logback/src/main/java/com/springboot/SpringbootLogbackApplication.java
@@ -1,10 +1,24 @@
 package com.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootLogbackApplication {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/test")
+    public void test() {
+        for (int i = 0; i < 2000; i++) {
+            log.error("hello");
+            log.info("hello");
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootLogbackApplication.class, args);
