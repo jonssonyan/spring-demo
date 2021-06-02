1f0a457fe727f23baa18151164713645100df950
YanZhuangZhuang <yz808@outlook.com>
2021-06-02 13:49:19 +0800

    Spring Boot Spring Kafka

--- springboot-springkafka/src/main/java/com/springboot/controller/MessageSenderController.java
+++ springboot-springkafka/src/main/java/com/springboot/controller/MessageSenderController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SendMessageController {
+public class MessageSenderController {
     @Autowired
     private KafkaTemplate<String, Message> kafkaTemplate;
 
