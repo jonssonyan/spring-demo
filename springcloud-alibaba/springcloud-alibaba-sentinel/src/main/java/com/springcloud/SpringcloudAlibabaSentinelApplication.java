8eaec5f92708dab053435bfb331c44c74dd07738
Jonsson <yz808@outlook.com>
2021-06-04 23:57:44 +0800

    Spring Cloud Alibaba Sentinel

---
+++ springcloud-alibaba/springcloud-alibaba-sentinel/src/main/java/com/springcloud/SpringcloudAlibabaSentinelApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudAlibabaSentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudAlibabaSentinelApplication.class, args);
+    }
+
+}
