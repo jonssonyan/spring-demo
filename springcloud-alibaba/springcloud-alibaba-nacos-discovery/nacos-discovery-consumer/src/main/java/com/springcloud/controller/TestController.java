5062ec85e57fbe75c94dba755f5ef331e474e8e3
Jonsson <yz808@outlook.com>
2021-06-05 21:25:45 +0800

    Spring Cloud Alibaba Nacos Discovery

--- springcloud-alibaba/springcloud-alibaba-nacos-discovery/nacos-discovery-consumer/src/main/java/com/springcloud/controller/TestController.java
+++ springcloud-alibaba/springcloud-alibaba-nacos-discovery/nacos-discovery-consumer/src/main/java/com/springcloud/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,7 +13,7 @@ public class TestController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
+    @GetMapping(value = "/echo/{str}")
     public String echo(@PathVariable String str) {
         return restTemplate.getForObject("http://service-provider/echo/" + str, String.class);
     }
--- springcloud-alibaba/springcloud-alibaba-nacos-discovery/nacos-discovery-provider/src/main/java/com/springcloud/controller/TestController.java
+++ springcloud-alibaba/springcloud-alibaba-nacos-discovery/nacos-discovery-provider/src/main/java/com/springcloud/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.springcloud.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+    @GetMapping(value = "/echo/{string}")
     public String echo(@PathVariable String string) {
         return "Hello Nacos Discovery " + string;
     }
