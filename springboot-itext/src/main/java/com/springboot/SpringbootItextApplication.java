ab11fac7e0967e121fa64f545ab39b2963bbc25e
YanZhuangZhuang <yz808@outlook.com>
2021-10-26 15:26:26 +0800

    Spring Boot iText

---
+++ springboot-itext/src/main/java/com/springboot/SpringbootItextApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootItextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootItextApplication.class, args);
+    }
+
+}
