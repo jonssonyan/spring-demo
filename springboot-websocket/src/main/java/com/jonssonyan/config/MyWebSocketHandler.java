28ce19b5a6263f88432f52c0eed5c847b4401f96
YanZhuangZhuang <yz808@outlook.com>
2021-11-02 17:22:42 +0800

    Spring Boot WebSocket

--- springboot-websocket/src/main/java/com/jonssonyan/config/MyWebSocketHandler.java
+++ springboot-websocket/src/main/java/com/jonssonyan/config/MyWebSocketHandler.java
@@ -1,4 +1,4 @@
-package com.springboot.config;
+package com.jonssonyan.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- springboot-websocket/src/main/java/com/jonssonyan/config/WebSocketServerConfig.java
+++ springboot-websocket/src/main/java/com/jonssonyan/config/WebSocketServerConfig.java
@@ -1,4 +1,4 @@
-package com.springboot.config;
+package com.jonssonyan.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
