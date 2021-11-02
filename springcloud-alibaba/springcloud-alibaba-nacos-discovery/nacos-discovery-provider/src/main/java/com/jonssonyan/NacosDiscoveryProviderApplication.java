c3fc177a6e1a54ef068c88917913cef52fba8229
YanZhuangZhuang <yz808@outlook.com>
2021-11-02 18:10:08 +0800

    Spring Cloud Alibaba Nacos Discovery Provider

--- springcloud-alibaba/springcloud-alibaba-nacos-discovery/nacos-discovery-provider/src/main/java/com/jonssonyan/NacosDiscoveryProviderApplication.java
+++ springcloud-alibaba/springcloud-alibaba-nacos-discovery/nacos-discovery-provider/src/main/java/com/jonssonyan/NacosDiscoveryProviderApplication.java
@@ -1,9 +1,8 @@
-package com.springcloud;
+package com.jonssonyan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 // 开启服务注册发现功能
 @SpringBootApplication
