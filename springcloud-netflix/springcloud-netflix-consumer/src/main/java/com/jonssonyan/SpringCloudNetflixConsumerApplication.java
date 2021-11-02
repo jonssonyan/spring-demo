40188d50992bcbee93b1f1ca4f2362e10dec0d12
YanZhuangZhuang <yz808@outlook.com>
2021-11-02 18:12:05 +0800

    Spring Cloud Netflix Consumer

--- springcloud-netflix/springcloud-netflix-consumer/src/main/java/com/jonssonyan/SpringCloudNetflixConsumerApplication.java
+++ springcloud-netflix/springcloud-netflix-consumer/src/main/java/com/jonssonyan/SpringCloudNetflixConsumerApplication.java
@@ -1,4 +1,4 @@
-package com.spingcloud;
+package com.jonssonyan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- springcloud-netflix/springcloud-netflix-consumer/src/main/java/com/jonssonyan/controller/TestController.java
+++ springcloud-netflix/springcloud-netflix-consumer/src/main/java/com/jonssonyan/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.spingcloud.controller;
+package com.jonssonyan.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
