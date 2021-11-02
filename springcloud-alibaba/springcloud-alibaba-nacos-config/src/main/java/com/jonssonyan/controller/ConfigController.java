28073e033660dfb6d3cae193b77aa2e6cc3d878e
YanZhuangZhuang <yz808@outlook.com>
2021-11-02 18:11:30 +0800

    Spring Cloud Alibaba Nacos Config

--- springcloud-alibaba/springcloud-alibaba-nacos-config/src/main/java/com/jonssonyan/controller/ConfigController.java
+++ springcloud-alibaba/springcloud-alibaba-nacos-config/src/main/java/com/jonssonyan/controller/ConfigController.java
@@ -1,4 +1,4 @@
-package com.springcloud.controller;
+package com.jonssonyan.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
