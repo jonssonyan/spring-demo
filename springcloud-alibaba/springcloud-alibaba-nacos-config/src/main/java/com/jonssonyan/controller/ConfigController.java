6e37e798972915861a51335e95211ace55971a38
YanZhuangZhuang <yz808@outlook.com>
2021-11-03 10:35:49 +0800

    Spring Cloud Alibaba Nacos Config

--- springcloud-alibaba/springcloud-alibaba-nacos-config/src/main/java/com/jonssonyan/controller/ConfigController.java
+++ springcloud-alibaba/springcloud-alibaba-nacos-config/src/main/java/com/jonssonyan/controller/ConfigController.java
@@ -2,18 +2,18 @@ package com.jonssonyan.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/config")
 // 实现配置自动更新
 @RefreshScope
 public class ConfigController {
     @Value("${useLocalCache:false}")
     private boolean useLocalCache;
 
-    @RequestMapping("/get")
+    @GetMapping("/get")
     public boolean get() {
         return useLocalCache;
     }
