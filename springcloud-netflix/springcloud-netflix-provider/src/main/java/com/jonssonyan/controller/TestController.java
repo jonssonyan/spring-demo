0242fe74c7aa7e51fe5f97a8262e0c275e2377eb
YanZhuangZhuang <yz808@outlook.com>
2021-11-03 11:35:37 +0800

    Spring Cloud Netflix Provider

--- springcloud-netflix/springcloud-netflix-provider/src/main/java/com/jonssonyan/controller/TestController.java
+++ springcloud-netflix/springcloud-netflix-provider/src/main/java/com/jonssonyan/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello() {
-        return "hello world";
+        return "provider say hello world !";
     }
 }
