e2824bfcf7e81378e14528d7b8f9d5da9419e5db
jonssonyan <yz808@outlook.com>
2022-01-26 12:29:18 +0800

    Spring Boot Spring Data Redis

--- springboot-springdata-redis/src/main/java/com/jonssonyan/controller/Controller.java
+++ springboot-springdata-redis/src/main/java/com/jonssonyan/controller/Controller.java
@@ -5,15 +5,20 @@ import com.jonssonyan.entity.User;
 import com.jonssonyan.entity.vo.ResultVO;
 import com.jonssonyan.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileNotFoundException;
+
 @RestController
+@RequestMapping("/redis")
 public class Controller {
     @Autowired
     private RedisService redisService;
 
-    @PostMapping("/redis/set")
+    @PostMapping("/setValue")
     public ResultVO<Object> set() {
         User user = new User();
         user.setId(1L);
@@ -21,5 +26,16 @@ public class Controller {
         user.setAge(22);
         redisService.setValue(RedisKey.builder().prefix("user").suffix("wang").build(), user, 10000L);
         return ResultVO.success(redisService.getValue(RedisKey.builder().prefix("user").suffix("wang").build(), user.getClass()));
+    }
+
+    @PostMapping("/setFile")
+    public ResultVO<Object> setFile() {
+        RedisService.Files files = redisService.new Files();
+        try {
+            files.setFile(ResourceUtils.getFile("classpath:static/测试.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return ResultVO.success(files.getFile("测试"));
     }
 }
