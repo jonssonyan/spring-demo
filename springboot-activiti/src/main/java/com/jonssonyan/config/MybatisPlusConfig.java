4dec823277bea2b54bfd8ebb1cf179ca251177d4
jonssonyan <yz808@outlook.com>
2022-03-10 13:06:22 +0800

    init

--- springboot-activiti/src/main/java/com/jonssonyan/config/MybatisPlusConfig.java
+++ springboot-activiti/src/main/java/com/jonssonyan/config/MybatisPlusConfig.java
@@ -1,5 +1,6 @@
 package com.jonssonyan.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
 import org.springframework.context.annotation.Bean;
@@ -23,7 +24,7 @@ public class MybatisPlusConfig {
         // 开启 count 的 join 优化,只针对部分 left join
         paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
         // 方言
-        paginationInterceptor.setDialectType("mysql");
+        paginationInterceptor.setDbType(DbType.MYSQL);
         return paginationInterceptor;
     }
 }
