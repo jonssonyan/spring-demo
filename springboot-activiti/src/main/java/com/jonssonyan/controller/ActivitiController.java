b16640d825869f84848d4945bdbe64e1b3a569ea
jonssonyan <yz808@outlook.com>
2022-03-23 13:36:58 +0800

    Spring Boot Activiti

--- springboot-activiti/src/main/java/com/jonssonyan/controller/ActivitiController.java
+++ springboot-activiti/src/main/java/com/jonssonyan/controller/ActivitiController.java
@@ -9,12 +9,16 @@ import org.activiti.engine.HistoryService;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.history.HistoricActivityInstance;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -53,6 +57,41 @@ public class ActivitiController {
         log.info("流程启动成功，流程id: {}", processInstance.getId());
         log.info("流程启动成功，流程definitionName: {}", processInstance.getProcessDefinitionName());
         log.info("流程启动成功，流程businessKey: {}", processInstance.getBusinessKey());
+        return Result.success();
+    }
+
+    /**
+     * 查询待办
+     *
+     * @return
+     */
+    @PostMapping("/getTaskList")
+    public Result getTaskList() {
+        List<HistoricActivityInstance> historicActivityInstances = historyService.createHistoricActivityInstanceQuery().list();
+        return Result.success(historicActivityInstances);
+    }
+
+    /**
+     * 完成任务
+     *
+     * @param taskId 流程id
+     * @return
+     */
+    @PostMapping("/completeByTaskById")
+    public Result completeTaskById(@RequestParam String taskId) {
+        taskService.complete(taskId);
+        return Result.success();
+    }
+
+    /**
+     * 结束流程
+     *
+     * @param taskId 流程id
+     * @return
+     */
+    @PostMapping("/deleteByTaskById")
+    public Result deleteProcessInstance(@RequestParam String taskId) {
+        taskService.deleteTask(taskId);
         return Result.success();
     }
 }
