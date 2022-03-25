f57976f2f19df297328c9a1bdc0f0e7c153248f4
jonssonyan <yz808@outlook.com>
2022-03-25 18:31:25 +0800

    Spring Boot Flowable

--- springboot-flowable/src/main/java/com/jonssonyan/config/FlowableConfig.java
+++ springboot-flowable/src/main/java/com/jonssonyan/config/FlowableConfig.java
@@ -1,8 +1,37 @@
 package com.jonssonyan.config;
 
+import org.flowable.spring.SpringProcessEngineConfiguration;
+import org.flowable.spring.boot.EngineConfigurationConfigurer;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
+import javax.sql.DataSource;
+
+/**
+ * flowable配置类
+ */
 @Configuration
-public class FlowableConfig {
+public class FlowableConfig implements EngineConfigurationConfigurer<SpringProcessEngineConfiguration> {
 
+    /**
+     * 解决flowable图片中的中文乱码
+     *
+     * @param springProcessEngineConfiguration
+     */
+    @Override
+    public void configure(SpringProcessEngineConfiguration springProcessEngineConfiguration) {
+        springProcessEngineConfiguration.setActivityFontName("宋体");
+        springProcessEngineConfiguration.setLabelFontName("宋体");
+        springProcessEngineConfiguration.setAnnotationFontName("宋体");
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource flowableDataSource() {
+        return DataSourceBuilder.create().build();
+    }
 }
