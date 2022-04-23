bb7803dbefc3aa02b93e4a2cfe48130b2a25c9ea
jonssonyan <yz808@outlook.com>
2022-04-23 16:30:25 +0800

    Spring Boot FreeMarker Vue2

--- springboot-freemarker-vue2/src/main/java/com/jonssonyan/controller/RouterController.java
+++ springboot-freemarker-vue2/src/main/java/com/jonssonyan/controller/RouterController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RouterController {
-    @GetMapping("/index")
-    public String index() {
-        return "index";
+    @GetMapping("/userList")
+    public String userList() {
+        return "userList";
     }
 }
--- springboot-freemarker-vue3/src/main/resources/templates/base.ftl
+++
@@ -1,4 +0,0 @@
-<script src="/static/js/axios.min.js"></script>
-<script src="/static/js/vue@next.min.js"></script>
-<script src="/static/js/antd.min.js"></script>
-<link rel="stylesheet" href="/static/css/antd.min.css" type="text/css">
