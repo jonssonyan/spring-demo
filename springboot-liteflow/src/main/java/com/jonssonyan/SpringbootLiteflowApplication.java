6ee9f5f94b3010b85d7eb3331a0c12f6efcba71c
jonssonyan <yz808@outlook.com>
2022-05-18 17:19:32 +0800

    Spring Boot LiteFlow

---
+++ springboot-liteflow/src/main/java/com/jonssonyan/SpringbootLiteflowApplication.java
@@ -0,0 +1,13 @@
+package com.jonssonyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLiteflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLiteflowApplication.class, args);
+    }
+
+}
