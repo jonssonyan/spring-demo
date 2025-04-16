5c6e5a8c4618d7bddb10b679871e87d27bf1a4a9
jonssonyan <yz808@outlook.com>
2025-04-16 10:30:53 +0800

    Spring Cloud Gateway

---
+++ springcloud-gateway/README.md
@@ -0,0 +1,7 @@
+# SpringCloud Gateway
+
+## 环境变量
+
+```
+CFG_ADDR=http://127.0.0.1:8848;
+```
---
+++ springcloud-gateway/springcloud-gateway-user/src/main/java/com/example/UserApplication.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+
+    @GetMapping("who")
+    public String ping() {
+        return "this is user";
+    }
+}
